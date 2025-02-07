1843dac24c0fc94ae1ec37d2663ca3be760845fa
Thiago Paiva <thiago.paiva@urbana-pe.com.br>
2025-02-07 14:18:17 -0300

    Criar RESTful para Lesson com validações #F039

--- src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -45,4 +45,10 @@ public class ModuleServiceImpl implements ModuleService {
     public List<ModuleModel> findAllByCourse(UUID courseId) {
         return moduleRepository.findAllModulesIntoCourse(courseId);
     }
+
+    @Override
+    public Optional<ModuleModel> findById(UUID moduleId) {
+        return moduleRepository.findById(moduleId);
+    }
+
 }
